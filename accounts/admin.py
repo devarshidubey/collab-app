8dda8147712958fcb94d777ab76b2cfc887645bc
Devarshi <devdubey7582@gmail.com>
2023-02-17 22:39:50 +0530

    created the whole user model and form

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'first_name', 'email', 'age', 'college', 'is_staff',]
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('age', 'college',)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('age', 'college',)}),)
+
+    
 # Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
