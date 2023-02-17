9031190f178c82a75972a88632303233c73fda70
Vishal Kumar <21bcs133@iiitdwd.ac.in>
2023-02-18 00:15:56 +0530

    "Login/Logout added"

--- collaboration_app/settings.py
+++ collaboration_app/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'collaboration_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))], # new
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,6 +71,10 @@ TEMPLATES = [
         },
     },
 ]
+
+############        Added Redirection url       ***************
+LOGIN_REDIRECT_URL = 'home' # new
+LOGOUT_REDIRECT_URL = 'home' # new
 
 WSGI_APPLICATION = 'collaboration_app.wsgi.application'
 
